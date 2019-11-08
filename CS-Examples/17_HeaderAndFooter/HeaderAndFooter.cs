4345b62588fec54a65c358769dde603dad1b0291
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:46:31 +0800

    Create HeaderAndFooter.cs

---
+++ CS-Examples/17_HeaderAndFooter/HeaderAndFooter.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Spire.Doc;
+using Spire.Doc.Documents;
+using Spire.Doc.Fields;
+
+namespace HeaderAndFooter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create word document
+            Document document = new Document();
+
+            document.LoadFromFile(@"..\..\..\..\..\..\Data\Sample.docx");
+            Section section = document.Sections[0];
+
+            //insert header and footer
+            InsertHeaderAndFooter(section);
+
+            //Save as docx file.
+            document.SaveToFile("Sample.docx",FileFormat.Docx);
+
+            //Launching the MS Word file.
+            WordDocViewer("Sample.docx");
+        }
+
+        private void InsertHeaderAndFooter(Section section)
+        {
+            HeaderFooter header = section.HeadersFooters.Header;
+            HeaderFooter footer = section.HeadersFooters.Footer;
+
+            //insert picture and text to header
+            Paragraph headerParagraph = header.AddParagraph();
+            DocPicture headerPicture
+                = headerParagraph.AppendPicture(Image.FromFile(@"..\..\..\..\..\..\Data\Header.png"));
+
+            //header text
+            TextRange text = headerParagraph.AppendText("Demo of Spire.Doc");
+            text.CharacterFormat.FontName = "Arial";
+            text.CharacterFormat.FontSize = 10;
+            text.CharacterFormat.Italic = true;
+            headerParagraph.Format.HorizontalAlignment
+                = Spire.Doc.Documents.HorizontalAlignment.Right;
+
+            //border
+            headerParagraph.Format.Borders.Bottom.BorderType
+                = Spire.Doc.Documents.BorderStyle.Single;
+            headerParagraph.Format.Borders.Bottom.Space = 0.05F;
+
+
+            //header picture layout - text wrapping
+            headerPicture.TextWrappingStyle = TextWrappingStyle.Behind;
+
+            //header picture layout - position
+            headerPicture.HorizontalOrigin = HorizontalOrigin.Page;
+            headerPicture.HorizontalAlignment = ShapeHorizontalAlignment.Left;
+            headerPicture.VerticalOrigin = VerticalOrigin.Page;
+            headerPicture.VerticalAlignment = ShapeVerticalAlignment.Top;
+
+            //insert picture to footer
+            Paragraph footerParagraph = footer.AddParagraph();
+            DocPicture footerPicture
+                = footerParagraph.AppendPicture(Image.FromFile(@"..\..\..\..\..\..\Data\Footer.png"));
+
+            //footer picture layout
+            footerPicture.TextWrappingStyle = TextWrappingStyle.Behind;
+            footerPicture.HorizontalOrigin = HorizontalOrigin.Page;
+            footerPicture.HorizontalAlignment = ShapeHorizontalAlignment.Left;
+            footerPicture.VerticalOrigin = VerticalOrigin.Page;
+            footerPicture.VerticalAlignment = ShapeVerticalAlignment.Bottom;
+
+            //insert page number
+            footerParagraph.AppendField("page number", FieldType.FieldPage);
+            footerParagraph.AppendText(" of ");
+            footerParagraph.AppendField("number of pages", FieldType.FieldNumPages);
+            footerParagraph.Format.HorizontalAlignment
+                = Spire.Doc.Documents.HorizontalAlignment.Right;
+
+            //border
+            footerParagraph.Format.Borders.Top.BorderType
+                = Spire.Doc.Documents.BorderStyle.Single;
+            footerParagraph.Format.Borders.Top.Space = 0.05F;
+        }
+
+        private void WordDocViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+
+    }
+}
