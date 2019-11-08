b28fe4be19c9773c72fe9b327067b06bed08264f
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:37:46 +0800

    Create GetText.cs

---
+++ CS-Examples/06_Text/GetText.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Doc;
+using System.IO;
+
+namespace GetText
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   
+			//Load the document from disk.
+            Document document = new Document(@"..\..\..\..\..\..\Data\ExtractText.docx");
+
+            //get text from document
+            string text = document.GetText();
+
+            //create a new TXT File to save extracted text
+            File.WriteAllText("Extract.txt", text);
+
+            //launch the file.
+            WordDocViewer("Extract.txt");
+        }
+
+        private void WordDocViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
