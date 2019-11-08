ed69e778f80082ba0fd44640fea69e3a57bc65fb
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:38:28 +0800

    Create SetWordViewModes.cs

---
+++ CS-Examples/07_DocumentOperation/SetWordViewModes.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Doc;
+using Spire.Doc.Documents;
+
+namespace SetWordViewModes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create Word document.
+            Document document = new Document();
+
+            //Load the file from disk.
+            document.LoadFromFile(@"..\..\..\..\..\..\Data\Template_Docx_1.docx");
+
+            //Set Word view modes.
+            document.ViewSetup.DocumentViewType = DocumentViewType.WebLayout;
+            document.ViewSetup.ZoomPercent = 150;
+            document.ViewSetup.ZoomType = ZoomType.None;
+
+            String result = "Result-SetWordViewModes.docx";
+
+            //Save to file.
+            document.SaveToFile(result, FileFormat.Docx2013);
+
+            //Launch the MS Word file.
+            WordDocViewer(result);
+        }
+
+        private void WordDocViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
