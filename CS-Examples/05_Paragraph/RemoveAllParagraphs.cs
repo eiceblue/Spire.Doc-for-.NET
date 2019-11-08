b0904cbb61de43b41613673624e30a7f52a0a600
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:36:21 +0800

    Create RemoveAllParagraphs.cs

---
+++ CS-Examples/05_Paragraph/RemoveAllParagraphs.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Doc;
+using Spire.Doc.Documents;
+
+namespace RemoveAllParagraphs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create Word document.
+            Document document = new Document();
+
+            //Load the file from disk.
+            document.LoadFromFile(@"..\..\..\..\..\..\Data\Template_Docx_1.docx");
+
+            //Remove paragraphs from every section in the document
+            foreach (Section section in document.Sections)
+            {
+                section.Paragraphs.Clear();
+            }
+
+            String result = "Result-RemoveAllParagraphs.docx";
+
+            //Save to file.
+            document.SaveToFile(result, FileFormat.Docx2013);
+
+            //Launch the MS Word file.
+            WordDocViewer(result);
+        }
+
+        private void WordDocViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
