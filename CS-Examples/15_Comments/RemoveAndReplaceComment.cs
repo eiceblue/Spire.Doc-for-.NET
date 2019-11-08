de91572eb0546a6189e8823ef37a8d4559431196
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:45:21 +0800

    Create RemoveAndReplaceComment.cs

---
+++ CS-Examples/15_Comments/RemoveAndReplaceComment.cs
@@ -0,0 +1,42 @@
+﻿using Spire.Doc;
+using System;
+using System.Windows.Forms;
+
+
+namespace RemoveAndReplaceComment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Load the document
+            string input = @"..\..\..\..\..\..\Data\CommentSample.docx";
+            Document doc = new Document();
+            doc.LoadFromFile(input);
+
+            //Replace the content of the first comment
+            doc.Comments[0].Body.Paragraphs[0].Replace("This is the title", "This comment is changed.", false, false);
+
+            //Remove the second comment
+            doc.Comments.RemoveAt(1);
+
+            //Save and launch
+            string output = "RemoveAndReplaceComment.docx";
+            doc.SaveToFile(output, FileFormat.Docx);
+            Viewer(output);
+        }
+        private void Viewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
