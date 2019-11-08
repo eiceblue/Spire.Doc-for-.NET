853ea02e65b8ba2b8cff731002f3996c1804a068
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:39:58 +0800

    Create ToEpub.cs

---
+++ CS-Examples/10_Conversion/ToEpub.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Doc;
+
+namespace ToEpub
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Document doc = new Document();
+            doc.LoadFromFile(@"..\..\..\..\..\..\..\Data\ToEpub.doc");
+            string result = "result.epub";
+            doc.SaveToFile(result, FileFormat.EPub);
+            WordDocViewer(result);
+        }
+        private void WordDocViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
