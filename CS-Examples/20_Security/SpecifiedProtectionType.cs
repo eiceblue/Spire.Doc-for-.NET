4b72a51558e43b7932d68a35b1a830cebc55896a
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:48:25 +0800

    Create SpecifiedProtectionType.cs

---
+++ CS-Examples/20_Security/SpecifiedProtectionType.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Doc;
+using Spire.Doc.Documents;
+
+namespace SpecifiedProtectionType
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create Word document.
+            Document document = new Document();
+
+            //Load the file from disk.
+            document.LoadFromFile(@"..\..\..\..\..\..\Data\Template_Docx_2.docx");
+
+            //Protect the Word file.
+            document.Protect(ProtectionType.AllowOnlyReading, "123456");
+
+            String result = "Result-SpecifiedProtectionType.docx";
+
+            //Save to file.
+            document.SaveToFile(result, FileFormat.Docx2013);
+
+            //Launch the file.
+            WordDocViewer(result);
+        }
+
+        private void WordDocViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
