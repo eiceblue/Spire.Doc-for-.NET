b9d67765ae0fff459c14eab05b10e7d63c897590
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:37:53 +0800

    Create CloneWordDocument.cs

---
+++ CS-Examples/07_DocumentOperation/CloneWordDocument.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Doc;
+using Spire.Doc.Documents;
+
+namespace CloneWordDocument
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create Word document.
+            Document document = new Document();
+
+            //Load the file from disk.
+            document.LoadFromFile(@"..\..\..\..\..\..\..\Data\Template_Docx_1.docx");
+
+            //Clone the word document.
+            Document newDoc = document.Clone();
+
+            String result = "Result-CloneWordDocument.docx";
+
+            //Save the file.
+            newDoc.SaveToFile(result, FileFormat.Docx2013);
+
+            //Launch the MS Word file.
+            WordDocViewer(result);
+        }
+
+        private void WordDocViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
