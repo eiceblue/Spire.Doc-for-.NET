3d2cd88dfa8459ea562d7cc09a08d82a2f71c48c
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:46:51 +0800

    Create RemoveHeader.cs

---
+++ CS-Examples/17_HeaderAndFooter/RemoveHeader.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Spire.Doc;
+using Spire.Doc.Documents;
+
+namespace RemoveHeader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Load the document
+            string input = @"..\..\..\..\..\..\Data\HeaderAndFooter.docx";            
+            Document doc = new Document();
+            doc.LoadFromFile(input);
+
+            //Get the first section of the document
+            Section section = doc.Sections[0];
+
+            //Traverse the word document and clear all headers in different type
+            foreach (Paragraph para in section.Paragraphs)
+            {
+                foreach (DocumentObject obj in para.ChildObjects)
+                {
+                    //Clear footer in the first page
+                    HeaderFooter header;
+                    header = section.HeadersFooters[HeaderFooterType.HeaderFirstPage];
+                    if (header != null)
+                        header.ChildObjects.Clear();
+                    //Clear footer in the odd page
+                    header = section.HeadersFooters[HeaderFooterType.HeaderOdd];
+                    if (header != null)
+                        header.ChildObjects.Clear();
+                    //Clear footer in the even page
+                    header = section.HeadersFooters[HeaderFooterType.HeaderEven];
+                    if (header != null)
+                        header.ChildObjects.Clear();
+                }
+            }
+
+            //Save and launch document
+            string output = "RemoveHeader.docx";
+            doc.SaveToFile(output, FileFormat.Docx);
+            Viewer(output);
+        }
+        private void Viewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
