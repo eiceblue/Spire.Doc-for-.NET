218a4bfe3916dabee38f11af91fb5066e2720494
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:50:45 +0800

    Create GetMathEquation.cs

---
+++ CS-Examples/27_MathEquations/GetMathEquation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Doc;
+using Spire.Doc.Fields.OMath;
+using Spire.Doc.Documents;
+using System.IO;
+namespace GetMathEquation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Document doc = new Document();
+            doc.LoadFromFile(@"..\..\..\..\..\..\Data\GetMathEquation.docx");
+            List<OfficeMath> mathEquations = new List<OfficeMath>();
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (Section section in doc.Sections)
+            {
+                foreach (Paragraph paragraph in section.Paragraphs)
+                {
+                    foreach (DocumentObject obj in paragraph.ChildObjects)
+                    {
+                        if (obj is OfficeMath)
+                        {
+                            stringBuilder.AppendLine((obj as OfficeMath).ToMathMLCode());
+                            stringBuilder.AppendLine();
+                            mathEquations.Add(obj as OfficeMath);
+                        }
+                    }
+
+                }
+            }
+            string output ="MathMLCode.txt";
+            File.WriteAllText(output, stringBuilder.ToString());
+            WordDocViewer(output);
+        }
+        private void WordDocViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
