10ed1fed9515f0e39c5ef2cc6c1509d0d16f921a
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:41:21 +0800

    Create WordToTxt.cs

---
+++ CS-Examples/10_Conversion/WordToTxt.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Doc;
+using Spire.Doc.Documents;
+
+namespace WordToTxt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create Word document.
+            Document document = new Document();
+
+            //Load the file from disk.
+            document.LoadFromFile(@"..\..\..\..\..\..\Data\Template_Docx_1.docx");
+
+            String result = "Result-WordToText.txt";
+
+            //Save the file.
+            document.SaveToFile(result, FileFormat.Txt);
+
+            //Launch the MS Word file.
+            WordDocViewer(result);
+        }
+
+        private void WordDocViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
