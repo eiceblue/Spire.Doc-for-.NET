e0a7c6930b2969c51fa72656e30ffc95eb65eed6
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:41:11 +0800

    Create ToXPS.cs

---
+++ CS-Examples/10_Conversion/ToXPS.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Doc;
+using Spire.Doc.Documents;
+using Spire.Doc.Fields;
+
+namespace ToXPS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create word document
+            Document document = new Document();
+            document.LoadFromFile(@"..\..\..\..\..\..\Data\ConvertedTemplate.docx");
+
+            //Save the document to a xps file.
+            document.SaveToFile("Sample.xps", FileFormat.XPS);
+
+            //Launching the pdf reader to open.
+            FileViewer("Sample.xps");
+        }
+
+        private void FileViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+
+    }
+}
