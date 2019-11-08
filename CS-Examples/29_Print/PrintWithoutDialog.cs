136df228ca9c157bcb1a24d1c8a193c84a0676f4
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:51:02 +0800

    Create PrintWithoutDialog.cs

---
+++ CS-Examples/29_Print/PrintWithoutDialog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Spire.Doc;
+using Spire.Doc.Documents;
+using System.Drawing.Printing;
+
+namespace PrintWithoutDialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Load the document
+            string input = @"..\..\..\..\..\..\Data\Sample.docx";
+            Document doc = new Document();
+            doc.LoadFromFile(input);
+
+            //Get the PrintDocument object
+            PrintDocument printDoc = doc.PrintDocument;
+
+            //Add the property PrintController to hide the print processing dialog
+            printDoc.PrintController = new StandardPrintController();
+
+            //Print the word document
+            printDoc.Print();
+        }
+    }
+}
