1a5356e3b6a64ecdb4884b7eccb4ae75162ed5be
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:39:36 +0800

    Create OdtToWord.cs

---
+++ CS-Examples/10_Conversion/OdtToWord.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Doc;
+using Spire.Doc.Documents;
+
+namespace OdtToWord
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create Word document.
+            Document document = new Document();
+
+            //Load the file from disk.
+            document.LoadFromFile(@"..\..\..\..\..\..\Data\Template_OdtFile.odt");
+
+            String result = "Result-OdtToDocOrDocx.docx";
+
+            //Save to Docx file.
+            document.SaveToFile(result, FileFormat.Docx);
+
+            //Launch the files.
+            WordDocViewer(result);
+        }
+
+        private void WordDocViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
