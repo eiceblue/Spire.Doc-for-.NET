61d1d7de84949d330e838a9a10b722f298f49a0f
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:39:38 +0800

    Create RtfToHtml.cs

---
+++ CS-Examples/10_Conversion/RtfToHtml.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Doc;
+using Spire.Doc.Documents;
+
+namespace RtfToHtml
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create Word document.
+            Document document = new Document();
+
+            //Load the file from disk.
+            document.LoadFromFile(@"..\..\..\..\..\..\Data\Template_RtfFile.rtf");
+
+            String result = "Result-RtfToHtml.html";
+
+            //Save to file.
+            document.SaveToFile(result, FileFormat.Html);
+
+            //Launch the Html file.
+            WordDocViewer(result);
+        }
+
+        private void WordDocViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
