3c2d339a2cf19b17e93c87d4161dd8aa09798d01
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:47:28 +0800

    Create CreateTableFromHTML.cs

---
+++ CS-Examples/18_Tables/CreateTableFromHTML.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Doc;
+using System.IO;
+
+namespace CreateTableFromHTML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //HTML string
+            String HTML = "<table border='2px'>" +
+                               "<tr>" +
+                               "<td>Row 1, Cell 1</td>" +
+                               "<td>Row 1, Cell 2</td>" +
+                               "</tr>" +
+                               "<tr>" +
+                               "<td>Row 2, Cell 2</td>" +
+                               "<td>Row 2, Cell 2</td>" +
+                               "</tr>" +
+                               "</table>";
+
+			//Create a Word document
+            Document document = new Document();
+
+            //Add a section
+            Section section = document.AddSection();
+
+            //Add a paragraph and append html string
+            section.AddParagraph().AppendHTML(HTML);
+
+            //Save to Word document
+            string output = "CreateTableFromHTML_out.docx";
+            document.SaveToFile(output, FileFormat.Docx2013);
+
+            //Launch the file
+            WordDocViewer(output);
+        }
+
+        private void WordDocViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
