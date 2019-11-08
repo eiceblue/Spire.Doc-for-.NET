1b4b49e055d0e36d4d6a7d9c3d55563794622993
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:49:10 +0800

    Create RemoveCustomPropertyFields.cs

---
+++ CS-Examples/21_Fields/RemoveCustomPropertyFields.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Doc;
+using Spire.Doc.Documents;
+
+namespace RemoveCustomPropertyFields
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create Word document.
+            Document document = new Document();
+
+            //Load the file from disk.
+            document.LoadFromFile(@"..\..\..\..\..\..\Data\RemoveCustomPropertyFields.docx");
+
+            //Get custom document properties object.
+            CustomDocumentProperties cdp = document.CustomDocumentProperties;
+
+            //Remove all custom property fields in the document.
+            for (int i = 0; i < cdp.Count; )
+            {
+                cdp.Remove(cdp[i].Name);
+            }
+
+            document.IsUpdateFields = true;
+
+            String result = "Result-RemoveCustomPropertyFields.docx";
+
+            //Save to file.
+            document.SaveToFile(result, FileFormat.Docx2013);
+
+            //Launch the MS Word file.
+            WordDocViewer(result);
+        }
+
+        private void WordDocViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
