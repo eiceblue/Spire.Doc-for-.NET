c8bd6621cbc80df48bae57dfc65bd2a1c485b0ed
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:46:27 +0800

    Create ImageHeaderAndFooter.cs

---
+++ CS-Examples/17_HeaderAndFooter/ImageHeaderAndFooter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using Spire.Doc;
+using Spire.Doc.Documents;
+using Spire.Doc.Fields;
+
+namespace ImageHeaderAndFooter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Load the document from disk
+            string input = @"..\..\..\..\..\..\Data\Template.docx";
+            Document doc = new Document();
+            doc.LoadFromFile(input);
+
+            //Get the header of the first page
+            HeaderFooter header = doc.Sections[0].HeadersFooters.Header;
+
+            //Add a paragraph for the header
+            Paragraph paragraph = header.AddParagraph();
+
+            //Set the format of the paragraph
+            paragraph.Format.HorizontalAlignment = Spire.Doc.Documents.HorizontalAlignment.Right;
+
+            //Append a picture in the paragraph
+            DocPicture headerimage = paragraph.AppendPicture(Image.FromFile(@"..\..\..\..\..\..\Data\E-iceblue.png"));
+            headerimage.VerticalAlignment = ShapeVerticalAlignment.Bottom;
+
+            //Get the footer of the first section
+            HeaderFooter footer = doc.Sections[0].HeadersFooters.Footer;
+
+            //Add a paragraph for the footer
+            Paragraph paragraph2 = footer.AddParagraph();
+
+            //Set the format of the paragraph
+            paragraph2.Format.HorizontalAlignment = Spire.Doc.Documents.HorizontalAlignment.Left;
+
+            //Append a picture in the paragraph
+            DocPicture footerimage = paragraph2.AppendPicture(Image.FromFile(@"..\..\..\..\..\..\Data\logo.png"));
+
+            //Append text in the paragraph
+            TextRange TR = paragraph2.AppendText("Copyright © 2013 e-iceblue. All Rights Reserved.");
+            TR.CharacterFormat.FontName = "Arial";
+            TR.CharacterFormat.FontSize = 10;
+            TR.CharacterFormat.TextColor = Color.Black;
+
+            //Save and launch document
+            string output = "ImageHeaderAndFooter.docx";
+            doc.SaveToFile(output, FileFormat.Docx);
+            Viewer(output);
+        }
+        private void Viewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
