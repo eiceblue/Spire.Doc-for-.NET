75176ba59d3a217b2ff8b373aeca9b981eb91046
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:49:48 +0800

    Create RemoveTextBox.cs

---
+++ CS-Examples/23_Textbox/RemoveTextBox.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+using Spire.Doc;
+
+namespace RemoveTextBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Load the document
+            string input = @"..\..\..\..\..\..\Data\TextBoxTemplate.docx";
+            Document doc = new Document();
+            doc.LoadFromFile(input);
+
+            //Remove the first text box
+            doc.TextBoxes.RemoveAt(0);
+
+            //Clear all the text boxes
+            //Doc.TextBoxes.Clear();
+
+            //Save and launch document
+            string output = "RemoveTextBox.docx";
+            doc.SaveToFile(output, FileFormat.Docx);
+            Viewer(output);
+        }
+        private void Viewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
