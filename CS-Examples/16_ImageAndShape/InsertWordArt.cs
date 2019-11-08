5b678748370fa95ed5128152787f7d40d6f4671c
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:45:51 +0800

    Create InsertWordArt.cs

---
+++ CS-Examples/16_ImageAndShape/InsertWordArt.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Doc;
+using Spire.Doc.Documents;
+using Spire.Doc.Fields;
+
+namespace InsertWordArt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {          
+            //Create Word document.
+            Document doc = new Document();
+
+            //Load Word document.
+            doc.LoadFromFile(@"..\..\..\..\..\..\Data\InsertWordArt.docx");
+
+            //Add a paragraph.
+            Paragraph paragraph = doc.Sections[0].AddParagraph();
+
+            //Add a shape.
+            ShapeObject shape = paragraph.AppendShape(250, 70, ShapeType.TextWave4);
+
+            //Set the position of the shape.
+            shape.VerticalPosition = 20;
+            shape.HorizontalPosition = 80;
+
+            //set the text of WordArt.
+            shape.WordArt.Text = "Thanks for reading.";
+
+            //Set the fill color.
+            shape.FillColor = System.Drawing.Color.Red;
+
+            //Set the border color of the text.
+            shape.StrokeColor = System.Drawing.Color.Yellow;
+
+            //Save docx file.
+            doc.SaveToFile("WordArt.docx", FileFormat.Docx2013);
+
+            //Launch the Word file.
+            FileViewer("WordArt.docx");
+        }
+        private void FileViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
