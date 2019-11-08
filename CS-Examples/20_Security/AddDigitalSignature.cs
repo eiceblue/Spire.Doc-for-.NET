53e3cc0823dd0d5e495440840beb0d87f50fc814
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:48:17 +0800

    Create AddDigitalSignature.cs

---
+++ CS-Examples/20_Security/AddDigitalSignature.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Doc;
+namespace AddDigitalSignature
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Document doc = new Document();
+            doc.LoadFromFile(@"..\..\..\..\..\..\Data\AddDigitalSignature.doc");
+            string result = "AddDigitalSignature_result.docx";
+            doc.SaveToFile(result, FileFormat.Docx, @"..\..\..\..\..\..\Data\gary.pfx", "e-iceblue");
+            WordDocViewer(result);
+        }
+        private void WordDocViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
