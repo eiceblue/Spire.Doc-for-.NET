97d2d28b5827416d2b724edf5a9db575c5aeee8d
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:47:45 +0800

    Create RemoveTable.cs

---
+++ CS-Examples/18_Tables/RemoveTable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Spire.Doc;
+using Spire.Doc.Documents;
+
+namespace RemoveTable
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Load the document
+            string input = @"..\..\..\..\..\..\Data\Template.docx";
+            Document doc = new Document();
+            doc.LoadFromFile(input);
+
+            //Remove the first Table            
+            doc.Sections[0].Tables.RemoveAt(0);
+
+            //Save and launch document
+            string output = "RemoveTable.docx";
+            doc.SaveToFile(output, FileFormat.Docx);
+            Viewer(output);
+        }
+        private void Viewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
