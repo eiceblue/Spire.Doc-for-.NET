a521fd371223d591f6fe958387754fb7f113f579
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:41:54 +0800

    Create GetTextByStyleName.cs

---
+++ CS-Examples/12_Styles/GetTextByStyleName.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Doc;
+using Spire.Doc.Documents;
+using System.IO;
+
+namespace GetTextByStyleName
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Load document from disk
+            Document doc = new Document();
+            doc.LoadFromFile(@"..\..\..\..\..\..\Data\Template_N5.docx");
+
+            //Create string builder
+            StringBuilder builder = new StringBuilder();
+
+            //Loop through sections
+            foreach (Section section in doc.Sections)
+            {
+                //Loop through paragraphs
+                foreach (Paragraph para in section.Paragraphs)
+                {
+                    //Find the paragraph whose style name is "Heading1"
+                    if (para.StyleName == "Heading1")
+                    {
+                        //Write the text of paragraph
+                        builder.AppendLine(para.Text);
+                    }
+                }
+            }
+
+            //Write the contents in a TXT file
+            string output="GetTextByStyleName_out.txt";
+            File.WriteAllText(output, builder.ToString());
+
+            //Launch the file
+            TxtViewer(output);
+        }
+        private void TxtViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+
+    }
+}
