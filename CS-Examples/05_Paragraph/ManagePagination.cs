eed7375ec762610f64ad0d42891a030403b08d56
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:36:17 +0800

    Create ManagePagination.cs

---
+++ CS-Examples/05_Paragraph/ManagePagination.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Doc;
+using Spire.Doc.Documents;
+
+namespace ManagePagination
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create Word document.
+            Document document = new Document();
+
+            //Load the file from disk.
+            document.LoadFromFile(@"..\..\..\..\..\..\Data\Template_Docx_1.docx");
+
+            //Get the first section and the paragraph we want to manage the pagination.
+            Section sec = document.Sections[0];
+            Paragraph para = sec.Paragraphs[4];
+
+            //Set the pagination format as Format.PageBreakBefore for the checked paragraph.
+            para.Format.PageBreakBefore = true;
+
+            String result = "Result-ManagePagination.docx";
+
+            //Save the file.
+            document.SaveToFile(result, FileFormat.Docx2013);
+
+            //Launch the MS Word file.
+            WordDocViewer(result);
+        }
+
+        private void WordDocViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
