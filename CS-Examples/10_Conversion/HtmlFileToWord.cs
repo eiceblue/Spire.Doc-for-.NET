a0d9988b8e61279bc08c8a45bb066220a03c8531
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:39:26 +0800

    Create HtmlFileToWord.cs

---
+++ CS-Examples/10_Conversion/HtmlFileToWord.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Doc;
+using Spire.Doc.Documents;
+
+namespace HtmlFileToWord
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Open an html file.
+            Document document = new Document();
+            document.LoadFromFile(@"..\..\..\..\..\..\..\Data\InputHtmlFile.html", FileFormat.Html, XHTMLValidationType.None);
+
+            //Save it to a Word document.
+            document.SaveToFile("HtmlFileToWord.docx", FileFormat.Docx);
+
+            //Launch the file.
+            WordDocViewer("HtmlFileToWord.docx");
+        }
+
+        private void WordDocViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
