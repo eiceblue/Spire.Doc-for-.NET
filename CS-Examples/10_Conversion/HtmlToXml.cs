585f309117fb0700d67b2045b9e2a4e46738505a
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:39:34 +0800

    Create HtmlToXml.cs

---
+++ CS-Examples/10_Conversion/HtmlToXml.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Doc;
+using Spire.Doc.Documents;
+
+namespace HtmlToXml
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create Word document.
+            Document document = new Document();
+
+            //Load the file from disk.
+            document.LoadFromFile(@"..\..\..\..\..\..\Data\Template_HtmlFile.html");
+
+            String result = "Result-HtmlToXml.xml";
+
+            //Save to file.
+            document.SaveToFile(result, FileFormat.Xml);
+
+            //Launch the file.
+            WordDocViewer(result);
+        }
+
+        private void WordDocViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
