8c0b9c263216ba951983bd76db25561c4e6b279c
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-11-08 15:33:29 +0800

    Create ReplaceWithDocument.cs

---
+++ CS-Examples/02_FindAndReplace/ReplaceWithDocument.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+using Spire.Doc;
+using Spire.Doc.Interface;
+
+namespace ReplaceWithDocument
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //Load a template document 
+            Document doc = new Document(@"..\..\..\..\..\..\Data\Text2.docx");
+
+            //Load another document to replace text
+            IDocument replaceDoc = new Document(@"..\..\..\..\..\..\Data\Text1.docx");
+
+            //Replace specified text with the other document
+            doc.Replace("Document1", replaceDoc, false, true);
+
+            //Save and launch document
+            string output = "ReplaceWithDocument.docx";
+            doc.SaveToFile(output, FileFormat.Docx);
+            Viewer(output);
+        }
+        private void Viewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
